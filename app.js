4bec50cd61ee6e1042890d0bc3c63636e1b7ae3e
shiyandgit <2309680542@qq.com>
2026-06-01 21:32:12 +0800

    v1.0.0：补充根目录配置文件

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ App({
   },
 
   onLaunch() {
+    // 初始化云开发
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        env: 'cloud1-d7gbglwc4912d7cab',
+        env: 'cloud1-d7gbglwc4912d7cab', // TODO: 替换为你的云开发环境ID，在云开发控制台顶部查看
         traceUser: true
       })
     }
 
+    // 检查登录状态
     const userInfo = wx.getStorageSync('userInfo')
     if (userInfo) {
       this.globalData.userInfo = userInfo
     }
   },
 
+  // 全局登录方法
   async login() {
     try {
       const res = await wx.cloud.callFunction({
