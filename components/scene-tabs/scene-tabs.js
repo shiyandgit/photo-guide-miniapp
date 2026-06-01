1aa706c77ce22cba21b9d685feeda0ec0f41f195
shiyandgit <2309680542@qq.com>
2026-06-01 19:47:54 +0800

    feat: 添加组件 - guide-card, scene-tabs, skeleton, custom-tab-bar

---
+++ components/scene-tabs/scene-tabs.js
@@ -0,0 +1,12 @@
+Component({
+  properties: {
+    scenes: { type: Array, value: [] },
+    selected: { type: String, value: '' }
+  },
+  methods: {
+    select(e) {
+      const scene = e.currentTarget.dataset.scene;
+      this.triggerEvent('select', { scene });
+    }
+  }
+});
---
+++ components/skeleton/index.js
@@ -0,0 +1,7 @@
+Component({
+  properties: {
+    loading: { type: Boolean, value: true },
+    type: { type: String, value: 'list' },
+    count: { type: Number, value: 3 }
+  }
+})
